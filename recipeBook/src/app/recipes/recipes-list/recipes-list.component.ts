2a90cd24c2e638b27181cc42ed08c26d0377aeba
HasanovH93 <hasanovh14@gmail.com>
2022-11-15 20:37:18 +0100

    set add to shopping-list

--- recipeBook/src/app/recipes/recipes-list/recipes-list.component.ts
+++ recipeBook/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -11,7 +11,7 @@ export class RecipesListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('Test Recipe','This is simply a Test', 'https://www.foodandwine.com/thmb/vVUv6lRmIZg9HgxnmemlK8ouD3I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Roasted-Butternut-Squash-with-chorizo-spiced-kale-FT-RECIPE0920-abdbe96ef511468d96b0cccca512ccaa.jpg'),
-    new Recipe('Test Recipe','This is simply a Test', 'https://www.foodandwine.com/thmb/vVUv6lRmIZg9HgxnmemlK8ouD3I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Roasted-Butternut-Squash-with-chorizo-spiced-kale-FT-RECIPE0920-abdbe96ef511468d96b0cccca512ccaa.jpg')
+    new Recipe('Another Test Recipe','This is simply a Test', 'https://www.foodandwine.com/thmb/vVUv6lRmIZg9HgxnmemlK8ouD3I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Roasted-Butternut-Squash-with-chorizo-spiced-kale-FT-RECIPE0920-abdbe96ef511468d96b0cccca512ccaa.jpg')
   ];
 
   constructor() { }
--- recipeBook/src/app/shopping-list/shopping-list.component.ts
+++ recipeBook/src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,9 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngridientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient)
+
+  }
+
 }
