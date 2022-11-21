c1f13f3a434a140beff1a1d85ffd67bbcc80d92f
HasanovH93 <hasanovh14@gmail.com>
2022-11-21 12:32:53 +0100

    add http requests to save and fetch data

--- recipeBook/src/app/header/header.component.ts
+++ recipeBook/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component} from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
     selector: 'app-header',
@@ -6,12 +7,14 @@ import { Component} from "@angular/core";
 })
 export class HeaderComponent {
 
-    
-  //  @Output() featureSelected = new EventEmitter<string>();
-    
+   constructor(private DataStorageService: DataStorageService){}
 
-    // onSelect(feature: string){
-    //   this.featureSelected.emit(feature)
-    // }
+    onSaveData(){
+    this.DataStorageService.storeRecipes()
+    }
+
+    onFetchData(){
+      this.DataStorageService.fetchRecipes()
+    }
 
 }
--- recipeBook/src/app/recipes/recipe.service.ts
+++ recipeBook/src/app/recipes/recipe.service.ts
@@ -9,29 +9,14 @@ import { Recipe } from './recipe.model';
 })
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>;
-
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'This is simply a Test',
-      'https://www.foodandwine.com/thmb/vVUv6lRmIZg9HgxnmemlK8ouD3I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Roasted-Butternut-Squash-with-chorizo-spiced-kale-FT-RECIPE0920-abdbe96ef511468d96b0cccca512ccaa.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries',2)
-      ]
-    ),
-    new Recipe(
-      'Burger',
-      'This is simply a Test',
-      'https://www.foodandwine.com/thmb/vVUv6lRmIZg9HgxnmemlK8ouD3I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Roasted-Butternut-Squash-with-chorizo-spiced-kale-FT-RECIPE0920-abdbe96ef511468d96b0cccca512ccaa.jpg',
-      [
-        new Ingredient('Onions', 1),
-        new Ingredient('Paper',4)
-      ]
-    ),
-  ];
-
+  private recipes : Recipe[]
+ 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice())
+  }
 
   getRecipes() {
     return this.recipes.slice();
