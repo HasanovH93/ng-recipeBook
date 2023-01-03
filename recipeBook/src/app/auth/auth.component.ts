6a4d25cad09ea66ea02d0f88557de5ec70db8a43
HasanovH93 <hasanovh14@gmail.com>
2023-01-03 17:30:38 +0100

    add auth form

--- recipeBook/src/app/auth/auth.component.ts
+++ recipeBook/src/app/auth/auth.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLogin = true;
+
+  onSwitchMode(){
+    console.log(this.isLogin)
+    this.isLogin = !this.isLogin
+  }
+
+  onSubmit(form:NgForm){
+    console.log(form.value)
+    form.reset()
+  }
+}
